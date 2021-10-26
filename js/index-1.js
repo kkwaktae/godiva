db30c64a5ed922c9a1695224e7068c2e6e26717e
kkwaktae <ender4@naver.com>
2021-10-26 17:11:46 +0900

    tap structure working

---
+++ js/index-1.js
@@ -0,0 +1,15 @@
+$(function(){
+	$(".page_03 .tab_content").hide();
+	$(".page_03 .tab_content").first().show();
+	
+	$(".page_03 li").click(function(){
+		$(".page_03 li").removeClass("active").css("color", "#fff");
+		$(this).addClass("active").css("color", "#f7e18a");
+		
+		$(".page_03 .tab_content").hide();
+		var activeTab = $(this).attr("rel");
+		
+		$("#"+activeTab).show();
+		
+	});
+});
