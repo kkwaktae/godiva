cafb8190e478e648c2a9296dac48dd1655a1b3c7
kkwaktae <ender4@naver.com>
2021-11-03 16:35:05 +0900

    main slide added

--- js/slide.js
+++ js/slide.js
@@ -25,20 +25,3 @@ $(function () {
   
   
 });
-
-$(function () {
-
-  var navSlider = $("#img_slide").bxSlider({
-    mode: "horizontal", // 수평방향으로 이동합니다
-    speed: 500, // 이동속도(500=0.5초)
-    pager: false, // 페이징표시제어 숨김
-    moveSlides: 1, // 이동 슬라이드 수
-    slideWidth: 200, // 각 슬라이드 폭
-    minSlides: 1, // 최소 노출 슬라이드 수
-    maxSlides: 1, // 최대 노출 슬라이드 수
-    auto: true, // 자동슬라이드 여부 (false 시 자동 x)
-    autoHover: false, // 마우스 오버시 자동 정지
-    controls: false, // 이전 다음 버튼을 숨긴다
-  });
-  
-});
