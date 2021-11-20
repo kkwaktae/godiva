edad6784cd497af472466117e4a76163bab345dc
kkwaktae <ender4@naver.com>
2021-11-20 16:32:36 +0900

    brand_page scroll action

--- js/brand_page/brand_page_scroll.js
+++ js/brand_page/brand_page_scroll.js
@@ -27,7 +27,6 @@ $(function(){
 		$('.effort_navi li a').removeClass('on');
 		$(this).find('a').addClass('on');
 		// 가로 페이저 css
-		
 	});
 	
 	$('.section_navi li').click(function(e){
@@ -43,12 +42,42 @@ $(function(){
 		}
 	});
 	
-	let pos1 = $('#section1').offset().top - 130 + 'px'
+	let pos1 = $('#section1').offset().top - 130
 	let pos2 = $('#section2').offset().top
 	let pos3 = $('#section3').offset().top
 	let pos4 = $('#section4').offset().top
 	let pos5 = $('#section5').offset().top
 	
+	let start = -500
+	
+	$(window).scroll(function(){
+		let scrT = $(window).scrollTop()
+		
+		if ( scrT >= pos1 + start && scrT < pos2 + start ) {
+			$('.section_navi li').find('a').removeClass('on')
+			$('.section_navi li').eq(0).find('a').addClass('on')
+		}
+		
+		if ( scrT >= pos2 + start && scrT < pos3 + start ) {
+			$('.section_navi li').find('a').removeClass('on')
+			$('.section_navi li').eq(1).find('a').addClass('on')
+		}
+		
+		if ( scrT >= pos3 + start && scrT < pos4 + start ) {
+			$('.section_navi li').find('a').removeClass('on')
+			$('.section_navi li').eq(2).find('a').addClass('on')
+		}
+		
+		if ( scrT >= pos4 + start && scrT < pos5 + start ) {
+			$('.section_navi li').find('a').removeClass('on')
+			$('.section_navi li').eq(3).find('a').addClass('on')
+		}
+		
+		if ( scrT >= pos5 + start ) {
+			$('.section_navi li').find('a').removeClass('on')
+			$('.section_navi li').eq(4).find('a').addClass('on')
+		}
+	});
 	
 	
 });
