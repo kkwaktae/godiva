7e08fde96ecbeb339521a3686ecd59536b0559ff
kkwaktae <ender4@naver.com>
2021-11-23 16:53:26 +0900

    mobile slide added

--- js/myslider.js
+++ js/myslider.js
@@ -3,3 +3,9 @@ $(window).on('load', function(){
 	$('.slider').slider({height : 850 + 'px'});
 	
 });
+
+$(window).on('load', function(){
+	
+	$('.slider_mobile').slider({height : 850 + 'px'});
+	
+});
