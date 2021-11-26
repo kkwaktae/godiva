b91af33998c4c79f31e6ff90d2236568c6b07290
kkwaktae <ender4@naver.com>
2021-11-27 03:01:08 +0900

    js files combinated

--- js/index_roll_gall_tab.js
+++ js/index_roll_gall_tab.js
@@ -1,8 +1,136 @@
+ //index js
+ $(function(){
+	//갤러리 이미지 베스트상품
+	
+	var list_zone = document.getElementById("photo_list1");
+	
+	var list_a = list_zone.getElementsByTagName("a");
+	
+	var pretxt ;
+	
+		for (let i = 0; i < list_a.length; i++) {
+			
+			list_a[i].onclick=function(){
+				
+				var ph = document.getElementById("photo_image1").children[0];
+				ph.src=this.href;
+				
+				var pt = document.getElementById("photo_text_box1");
+				
+				pt.children[0].style.display = "none";
+				
+				if (pretxt) {
+					pretxt.style.display = "none";
+				}
+				
+				pt.children[i].style.display = "block";
+				
+				for (let j = 0; j <pt.children.length; j++) {
+					
+					if (this.rel == pt.children[j].id) {
+						pretxt = pt.children[j] ;
+					}
+				}
+			
+				return false;
+			}
+		}	
+});
+
+$(function(){
+	//갤러리 이미지 시즌 한정
+	
+	var list_zone = document.getElementById("photo_list2");
+	
+	var list_a = list_zone.getElementsByTagName("a");
+	
+	var pretxt ;
+	
+		for (let i = 0; i < list_a.length; i++) {
+			
+			list_a[i].onclick=function(){
+				
+				var ph = document.getElementById("photo_image2").children[0];
+				ph.src=this.href;
+				
+				var pt = document.getElementById("photo_text_box2");
+				
+				pt.children[0].style.display = "none";
+				
+				if (pretxt) {
+					pretxt.style.display = "none";
+				}
+				
+				pt.children[i].style.display = "block";
+				
+				for (let j = 0; j <pt.children.length; j++) {
+					
+					if (this.rel == pt.children[j].id) {
+						pretxt = pt.children[j] ;
+					}
+				}
+			
+				return false;
+			}
+		}	
+});
+
+$(function(){
+	//갤러리 이미지 신메뉴
+	
+	var list_zone = document.getElementById("photo_list3");
+	
+	var list_a = list_zone.getElementsByTagName("a");
+	
+	var pretxt ;
+	
+		for (let i = 0; i < list_a.length; i++) {
+			
+			list_a[i].onclick=function(){
+				
+				var ph = document.getElementById("photo_image3").children[0];
+				ph.src=this.href;
+				
+				var pt = document.getElementById("photo_text_box3");
+				
+				pt.children[0].style.display = "none";
+				
+				if (pretxt) {
+					pretxt.style.display = "none";
+				}
+				
+				pt.children[i].style.display = "block";
+				
+				for (let j = 0; j <pt.children.length; j++) {
+					
+					if (this.rel == pt.children[j].id) {
+						pretxt = pt.children[j] ;
+					}
+				}
+			
+				return false;
+			}
+		}	
+});
+
 
  $(function(){
 	 //롤링 텍스트
 	 fn_article3("notice5", "bt5", true); 
 	 
+	 //탭구조
+	$(".page_03 .tab_content").hide();
+	$(".page_03 .tab_content").first().show();
+	
+	$(".page_03 li").click(function(){
+		$(".page_03 li").removeClass("active").css("color", "#fff");
+		$(this).addClass("active").css("color", "#f7e18a");
+		
+		$(".page_03 .tab_content").hide();
+		var activeTab = $(this).attr("rel");
+		
+		$("#"+activeTab).show();
+	});
  });
 
 
--- js/sliderk.js
+++ js/sliderk.js
@@ -1,3 +1,44 @@
+// index 대슬라이드 PC,Mobile 
+
+$(window).on('load', function(){
+	
+	$('.slider').slider();
+	$('.slider_mobile').slider();
+	
+});
+
+// 이벤트 슬라이드
+
+$(function () {
+
+  var mySlider = $("#event_con").bxSlider({
+    mode: "horizontal", // 수평방향으로 이동합니다
+    speed: 500, // 이동속도(500=0.5초)
+    pager: false, // 페이징표시제어 숨김
+    moveSlides: 1, // 이동 슬라이드 수
+    slideWidth: 750, // 각 슬라이드 폭
+    minSlides: 3, // 최소 노출 슬라이드 수
+    maxSlides: 3, // 최대 노출 슬라이드 수
+    auto: true, // 자동슬라이드 여부 (false 시 자동 x)
+    autoHover: true, // 마우스 오버시 자동 정지
+    controls: false, // 이전 다음 버튼을 숨긴다
+  });
+
+  $(".prev_btn").click(function () {
+    mySlider.goToPrevSlide();
+    return false;
+  });
+
+  $(".next_btn").click(function () {
+    mySlider.goToNextSlide();
+    return false;
+  });
+  
+  
+});
+
+
+
 /* global jQuery */
 (function ($) {
   'use strict'
