dda60f193252f0cf67e9893aff30a7c6f4f47e49
kkwaktae <ender4@naver.com>
2021-12-07 18:05:22 +0900

    each page linked

--- js/myslider.js
+++ js/myslider.js
@@ -1,24 +1,22 @@
 $(function(){
 	
-//	const slides = $('.slider li');
-
-//	$(window).resize(function(){	
-	
-//		let width=$(window).innerWidth();
-//		console.log(width);
+	const slides = $('.slider li');
+		$(window).resize(function(){	
 		
-//		if (width < 1023) {	
-//			for ( let i = 0 ; i < slides.length ; i++ ) {
-//				let imgNum = i+1
-//				slides.eq(i).find('img').attr('src','img/slide/slide_0'+imgNum+'_1024px.png');
-//			}
-//		} else {	
-//			for ( let i = 0 ; i < slides.length ; i++ ) {
-//				let imgNum = i+1
-//				slides.eq(i).find('img').attr('src','img/slide/slide_0'+imgNum+'.png');
-//			}
-//		}
-//	}).resize();
+		let width=$(window).innerWidth();	
+		
+		if (width < 1024) {	
+			for ( let i = 0 ; i < slides.length ; i++ ) {
+				let imgNum = i+1
+				slides.eq(i).find('img').attr('src','img/slide/slide_0'+imgNum+'_1024px.png');
+			}
+		} else {	
+			for ( let i = 0 ; i < slides.length ; i++ ) {
+				let imgNum = i+1
+				slides.eq(i).find('img').attr('src','img/slide/slide_0'+imgNum+'.png');
+			}
+		}
+	}).resize();
 	
 	$('.slider').slider();
 	
